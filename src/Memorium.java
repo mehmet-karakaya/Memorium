09c0c2fb1843098311998a90068e8c1593630300
Mehmet Karakaya <m.karakaya.github@gmail.com>
2024-10-19 17:14:00 +0300

    Updates

--- src/Memorium.java
+++ src/Memorium.java
@@ -43,17 +43,126 @@ public class Memorium {
     int boardHeight = rows * cardHeight; //4*90=360px
 
     JFrame frame = new JFrame("Memorium");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+    JPanel restartGamePanel = new JPanel();
+    JButton restartButton = new JButton();
 
+    int errorCount = 0;
+    ArrayList<JButton> board;
+    Timer hideCardTimer;
+    boolean gameReady = false;
+    JButton card1Selected;
+    JButton card2Selected;
 
     Memorium() {
         setupCards();
         shuffleCards();
 
-        frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Errors: " + Integer.toString(errorCount));
+
+        textPanel.setPreferredSize(new Dimension(boardWidth, 30));
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        //Card Game Board
+        board = new ArrayList<JButton>();
+        boardPanel.setLayout(new GridLayout(rows, columns));
+        for (int i = 0; i < cardSet.size(); i++) {
+            JButton tile = new JButton();
+            tile.setPreferredSize(new Dimension(cardWidth, cardHeight));
+            tile.setOpaque(true);
+            tile.setIcon(cardSet.get(i).cardImageIcon);
+            tile.setFocusable(false);
+            tile.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (!gameReady) {
+                        return;
+                    }
+                    JButton tile = (JButton) e.getSource();
+                    if (tile.getIcon() == cardBackImageIcon) {
+                        if (card1Selected == null) {
+                            card1Selected = tile;
+                            int index = board.indexOf(card1Selected);
+                            card1Selected.setIcon(cardSet.get(index).cardImageIcon);
+                        } else if (card2Selected == null) {
+                            card2Selected = tile;
+                            int index = board.indexOf(card2Selected);
+                            card2Selected.setIcon(cardSet.get(index).cardImageIcon);
+
+                            //Check if two cards are the same
+                            if (card1Selected.getIcon() != card2Selected.getIcon()) {
+                                errorCount += 1;
+                                textLabel.setText("Errors: " + Integer.toString(errorCount));
+                                hideCardTimer.start();
+                            } else {
+                                card1Selected = null;
+                                card2Selected = null;
+                            }
+                        }
+                    }
+                }
+            });
+            board.add(tile);
+            boardPanel.add(tile);
+        }
+        frame.add(boardPanel);
+
+        //Restart Game Button
+        restartButton.setFont(new Font("Arial", Font.PLAIN, 16));
+        restartButton.setText("Restart Game");
+        restartButton.setPreferredSize(new Dimension(boardWidth, 30));
+        restartButton.setFocusable(false);
+        restartButton.setEnabled(false);
+        restartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!gameReady) {
+                    return;
+                }
+                gameReady = false;
+                restartButton.setEnabled(false);
+                card1Selected = null;
+                card2Selected = null;
+                shuffleCards();
+
+                //Reassign Buttons with new Cards
+                for (int i = 0; i < board.size(); i++) {
+                    board.get(i).setIcon(cardSet.get(i).cardImageIcon);
+                }
+
+                errorCount = 0;
+                textLabel.setText("Errors: " + Integer.toString(errorCount));
+                hideCardTimer.start();
+            }
+        });
+        restartGamePanel.add(restartButton);
+        frame.add(restartGamePanel, BorderLayout.SOUTH);
+
+
+        frame.pack();
+        frame.setVisible(true);
+
+        //Start Game
+        hideCardTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideCards();
+            }
+        });
+        hideCardTimer.setRepeats(false);
+        hideCardTimer.start();
+
     }
 
     void setupCards() {
@@ -84,5 +193,20 @@ public class Memorium {
             cardSet.set(j, temp);
         }
         System.out.println(cardSet);
+    }
+
+    void hideCards() {
+        if (gameReady && card1Selected != null && card2Selected != null) {//Flip only the selected two cards face down
+            card1Selected.setIcon(cardBackImageIcon);
+            card1Selected = null;
+            card2Selected.setIcon(cardBackImageIcon);
+            card2Selected = null;
+        } else { //Flip all cards face down
+            for (int i = 0; i < board.size(); i++) {
+                board.get(i).setIcon(cardBackImageIcon);
+            }
+            gameReady = true;
+            restartButton.setEnabled(true);
+        }
     }
 }
